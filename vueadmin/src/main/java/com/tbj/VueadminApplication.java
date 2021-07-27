15a73b986b0d01e1cf34082680bb1418e5f073ae
Tbj <3062424868@qq.com>
2021-07-27 20:36:36 +0800

    Initial commit

---
+++ vueadmin/src/main/java/com/tbj/VueadminApplication.java
@@ -0,0 +1,13 @@
+package com.tbj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueadminApplication.class, args);
+    }
+
+}
---
+++ vueadmin/src/test/java/com/tbj/VueadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
